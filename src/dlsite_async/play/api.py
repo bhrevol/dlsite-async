fc02e17b02068fc8ab32c4969c2035182e64388b
byeonhyeok <bhrevol@gmail.com>
2022-10-01 16:13:03 +0900

    play: add download_playfile

--- src/dlsite_async/play/api.py
+++ src/dlsite_async/play/api.py
@@ -1,8 +1,15 @@
 """DLsite Play API classes."""
-from typing import Any
+import logging
+import os
+import tempfile
+from pathlib import Path
+from typing import Any, Union
 
 from ..api import BaseAPI
-from .models import DownloadToken, ZipTree
+from .models import DownloadToken, PlayFile, ZipTree
+
+
+logger = logging.getLogger(__name__)
 
 
 class PlayAPI(BaseAPI["PlayAPI"]):
@@ -45,3 +52,64 @@ class PlayAPI(BaseAPI["PlayAPI"]):
         url = f"{token.url}ziptree.json"
         async with self.get(url, params=token.params) as response:
             return ZipTree.from_json(await response.json())
+
+    async def download_playfile(
+        self,
+        token: DownloadToken,
+        playfile: PlayFile,
+        dest: Union[str, Path],
+        mkdir: bool = False,
+        force: bool = False,
+        descramble: bool = False,
+        **kwargs: Any,
+    ) -> None:
+        """Download playfile to the specified location.
+
+        Args:
+            token: A download token returned from `download_token`.
+            playfile: PlayFile to download.
+            dest: Destination path to write the downloaded file.
+            mkdir: Create ``dest`` parent directories if they do not already
+                exist.
+            force: Overwrite `dest` if it already exists.
+            descramble: Descramble downloaded images (requires optional
+                ``dlsite-async[pil]`` dependency packages).
+
+        Raises:
+            FileExistsError: ``dest`` already exists.
+        """
+        if isinstance(dest, str):
+            dest = Path(dest)
+        try:
+            url = f"{token.url}optimized/{playfile.optimized_name}"
+        except KeyError:
+            logger.warn(
+                f"Could not download {dest}: no web-optimized version available."
+            )
+            return
+        if mkdir and not dest.parent.exists():
+            dest.parent.mkdir()
+        if not force and dest.exists():
+            raise FileExistsError(str(dest))
+        async with self.get(
+            url, params=token.params, timeout=self._DL_TIMEOUT
+        ) as response:
+            try:
+                with tempfile.NamedTemporaryFile(
+                    prefix=dest.name, dir=dest.parent, delete=False
+                ) as temp:
+                    async for chunk in response.content.iter_chunked(
+                        self._DL_CHUNK_SIZE
+                    ):
+                        temp.write(chunk)
+            except Exception:
+                temp.close()
+                os.remove(temp)
+                raise
+        if (
+            playfile.type == "image"
+            and playfile.files["optimized"].get("crypt")
+            and descramble
+        ):
+            pass
+        os.replace(temp.name, dest)
