3d3c1be67b35170e65377f96cb3a55c20f4a6073
byeonhyeok <bhrevol@gmail.com>
2024-01-01 14:04:40 +0900

    ci: migrate nox/tests to pdm

--- noxfile.py
+++ noxfile.py
@@ -1,25 +1,14 @@
 """Nox sessions."""
 import os
-import shlex
 import shutil
 import sys
 from pathlib import Path
-from textwrap import dedent
 
 import nox
+from nox import Session, session
 
 
-try:
-    from nox_poetry import Session, session
-except ImportError:
-    message = f"""\
-    Nox failed to import the 'nox-poetry' package.
-
-    Please install it using the following command:
-
-    {sys.executable} -m pip install nox-poetry"""
-    raise SystemExit(dedent(message)) from None
-
+os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
 
 package = "dlsite_async"
 python_versions = ["3.11", "3.10", "3.9"]
@@ -35,130 +24,33 @@ nox.options.sessions = (
 )
 
 
-def activate_virtualenv_in_precommit_hooks(session: Session) -> None:
-    """Activate virtualenv in hooks installed by pre-commit.
-
-    This function patches git hooks installed by pre-commit to activate the
-    session's virtual environment. This allows pre-commit to locate hooks in
-    that environment when invoked from git.
-
-    Args:
-        session: The Session object.
-    """
-    assert session.bin is not None  # noqa: S101
-
-    # Only patch hooks containing a reference to this session's bindir. Support
-    # quoting rules for Python and bash, but strip the outermost quotes so we
-    # can detect paths within the bindir, like <bindir>/python.
-    bindirs = [
-        bindir[1:-1] if bindir[0] in "'\"" else bindir
-        for bindir in (repr(session.bin), shlex.quote(session.bin))
-    ]
-
-    virtualenv = session.env.get("VIRTUAL_ENV")
-    if virtualenv is None:
-        return
-
-    headers = {
-        # pre-commit < 2.16.0
-        "python": f"""\
-            import os
-            os.environ["VIRTUAL_ENV"] = {virtualenv!r}
-            os.environ["PATH"] = os.pathsep.join((
-                {session.bin!r},
-                os.environ.get("PATH", ""),
-            ))
-            """,
-        # pre-commit >= 2.16.0
-        "bash": f"""\
-            VIRTUAL_ENV={shlex.quote(virtualenv)}
-            PATH={shlex.quote(session.bin)}"{os.pathsep}$PATH"
-            """,
-        # pre-commit >= 2.17.0 on Windows forces sh shebang
-        "/bin/sh": f"""\
-            VIRTUAL_ENV={shlex.quote(virtualenv)}
-            PATH={shlex.quote(session.bin)}"{os.pathsep}$PATH"
-            """,
-    }
-
-    hookdir = Path(".git") / "hooks"
-    if not hookdir.is_dir():
-        return
-
-    for hook in hookdir.iterdir():
-        if hook.name.endswith(".sample") or not hook.is_file():
-            continue
-
-        if not hook.read_bytes().startswith(b"#!"):
-            continue
-
-        text = hook.read_text()
-
-        if not any(
-            Path("A") == Path("a") and bindir.lower() in text.lower() or bindir in text
-            for bindir in bindirs
-        ):
-            continue
-
-        lines = text.splitlines()
-
-        for executable, header in headers.items():
-            if executable in lines[0].lower():
-                lines.insert(1, dedent(header))
-                hook.write_text("\n".join(lines))
-                break
-
-
 @session(name="pre-commit", python=python_versions[0])
 def precommit(session: Session) -> None:
     """Lint using pre-commit."""
+    session.run_always("pdm", "install", "-G", "lint", external=True)
     args = session.posargs or [
         "run",
         "--all-files",
-        "--hook-stage=manual",
         "--show-diff-on-failure",
     ]
-    session.install(
-        "black",
-        "darglint",
-        "flake8",
-        "flake8-bandit",
-        "flake8-bugbear",
-        "flake8-docstrings",
-        "flake8-rst-docstrings",
-        "isort",
-        "pep8-naming",
-        "pre-commit",
-        "pre-commit-hooks",
-        "pyupgrade",
-    )
     session.run("pre-commit", *args)
-    if args and args[0] == "install":
-        activate_virtualenv_in_precommit_hooks(session)
 
 
 @session(python=python_versions[0])
 def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
-    requirements = session.poetry.export_requirements()
-    session.install("safety")
-    session.run("safety", "check", "--full-report", f"--file={requirements}")
+    session.run_always("pdm", "install", "-G", "safety", external=True)
+    session.run("pdm", "export", "-o", "requirements.txt", "--without-hashes")
+    session.run("safety", "check", "--full-report", "--file=requirements.txt")
 
 
 @session(python=python_versions[0])
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
-    args = session.posargs or ["src", "tests", "docs/conf.py"]
-    session.install(".[pil]")
-    session.install(
-        "aioresponses",
-        "lxml-stubs",
-        "mypy",
-        "pytest",
-        "pytest-asyncio",
-        "pytest-mock",
-        "types-pillow",
+    session.run_always(
+        "pdm", "install", "-G", "pil", "-G", "mypy", "-G", "tests", external=True
     )
+    args = session.posargs or ["src", "tests", "docs/conf.py"]
     session.run("mypy", *args)
     if not session.posargs:
         session.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
@@ -167,14 +59,16 @@ def mypy(session: Session) -> None:
 @session(python=python_versions)
 def tests(session: Session) -> None:
     """Run the test suite."""
-    session.install(".[pil]")
-    session.install(
-        "aioresponses",
-        "coverage[toml]",
-        "pygments",
-        "pytest",
-        "pytest-asyncio",
-        "pytest-mock",
+    session.run_always(
+        "pdm",
+        "install",
+        "-G",
+        "pil",
+        "-G",
+        "tests",
+        "-G",
+        "coverage",
+        external=True,
     )
     try:
         session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
@@ -186,9 +80,8 @@ def tests(session: Session) -> None:
 @session(python=python_versions[0])
 def coverage(session: Session) -> None:
     """Produce the coverage report."""
+    session.run_always("pdm", "install", "-G", "coverage", external=True)
     args = session.posargs or ["report"]
-
-    session.install("coverage[toml]")
 
     if not session.posargs and any(Path().glob(".coverage.*")):
         session.run("coverage", "combine")
@@ -199,14 +92,16 @@ def coverage(session: Session) -> None:
 @session(python=python_versions[0])
 def typeguard(session: Session) -> None:
     """Runtime type checking using Typeguard."""
-    session.install(".[pil]")
-    session.install(
-        "aioresponses",
-        "pygments",
-        "pytest",
-        "pytest-asyncio",
-        "pytest-mock",
+    session.run_always(
+        "pdm",
+        "install",
+        "-G",
+        "pil",
+        "-G",
         "typeguard",
+        "-G",
+        "tests",
+        external=True,
     )
     session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
 
@@ -214,6 +109,7 @@ def typeguard(session: Session) -> None:
 @session(python=python_versions)
 def xdoctest(session: Session) -> None:
     """Run examples with xdoctest."""
+    session.run_always("pdm", "install", "-G", "pil", "-G", "xdoctest", external=True)
     if session.posargs:
         args = [package, *session.posargs]
     else:
@@ -221,20 +117,16 @@ def xdoctest(session: Session) -> None:
         if "FORCE_COLOR" in os.environ:
             args.append("--colored=1")
 
-    session.install(".")
-    session.install("xdoctest[colors]")
     session.run("python", "-m", "xdoctest", *args)
 
 
 @session(name="docs-build", python=python_versions[0])
 def docs_build(session: Session) -> None:
     """Build the documentation."""
+    session.run_always("pdm", "install", "-G", "pil", "-G", "docs", external=True)
     args = session.posargs or ["docs", "docs/_build"]
     if not session.posargs and "FORCE_COLOR" in os.environ:
         args.insert(0, "--color")
-
-    session.install(".")
-    session.install("sphinx", "sphinx-click", "furo", "myst-parser")
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
@@ -246,9 +138,8 @@ def docs_build(session: Session) -> None:
 @session(python=python_versions[0])
 def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
+    session.run_always("pdm", "install", "-G", "pil", "-G", "docs", external=True)
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
-    session.install(".")
-    session.install("sphinx", "sphinx-autobuild", "sphinx-click", "furo", "myst-parser")
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
